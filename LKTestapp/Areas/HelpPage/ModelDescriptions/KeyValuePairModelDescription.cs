5ceed9bc464491071192a6b7082b5805137b2417
laavanya <laavanya.d@gmail.com>
2016-10-01 19:29:52 -0400

    Initial Commit

---
+++ LKTestapp/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace LKTestapp.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
